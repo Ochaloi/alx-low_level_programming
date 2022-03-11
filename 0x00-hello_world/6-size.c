fd630206e1515004ffcfbd9669370e74d0f5b0d6
Ochaloi <ochaloidaniel@gmail.com>
2022-03-11 01:53:16 -0800

    6print

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <srdio.h>
 
 /**
 * main - Entry point
@@ -8,15 +8,16 @@
 
 int main(void)
 {
-	har acharacter;
-        int ainteger;
-        long along;
-        long long alonglong;
+	char acharacter;
+	int ainteger;
+	long along;
+	long long alonglong;
 	float afloat;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
-        printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
-        printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+
+	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger))
+	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
 	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
 

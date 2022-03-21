c9799126e2a609b9c74aefb037a6fded9924a840
Ochaloi <ochaloidaniel@gmail.com>
2022-03-21 14:26:04 -0700

    done

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,19 @@
 #include "main.h"
+
 /**
-*_puts - prints a string, followed by a new line,
-* @str: pointer to the string to print
-* Return: void
-*/
+ * _puts - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
+ */
 
-
-void _puts.c(char *str)
+void _puts(char *str)
 {
 	int i = 0;
+
 	while (str[i])
-{
-	_putchar(str[i]);
-	i++;
-}
-_putchar('\n');
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,5 +2,3 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts.c(char *str);
-void rev_string(char *s);
-int _putchar(char c);
